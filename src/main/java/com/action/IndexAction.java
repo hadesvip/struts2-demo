bbe9e46b4edf6e04ee369c835d01fb8b191b8dec
wangyong <yundan_wy@126.com>
2017-09-23 00:13:35 +0800

    完善代码

--- src/main/java/com/action/IndexAction.java
+++ src/main/java/com/action/IndexAction.java
@@ -1,12 +1,14 @@
 package com.action;
 
 import com.action.base.BaseAction;
+import com.domain.Goods;
 import com.domain.User;
 import com.google.gson.Gson;
 import org.apache.struts2.ServletActionContext;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.List;
 
 public class IndexAction extends BaseAction {
 
@@ -18,6 +20,39 @@ public class IndexAction extends BaseAction {
 
     public IndexAction setUser(User user) {
         this.user = user;
+        return this;
+    }
+
+    private String userName;
+
+    private String email;
+
+    private List<Goods> goodsList;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public IndexAction setUserName(String userName) {
+        this.userName = userName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public IndexAction setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public List<Goods> getGoodsList() {
+        return goodsList;
+    }
+
+    public IndexAction setGoodsList(List<Goods> goodsList) {
+        this.goodsList = goodsList;
         return this;
     }
 
--- src/main/java/com/action/base/BaseAction.java
+++ src/main/java/com/action/base/BaseAction.java
@@ -7,6 +7,14 @@ import java.io.IOException;
 
 public class BaseAction extends ActionSupport {
 
+    /**
+     * 解析json请求数据
+     * 此方法用于没有使用struts2-json-plugin场景
+     *
+     * @param request 请求
+     * @return
+     * @throws IOException
+     */
     protected String parseReqJsonData(HttpServletRequest request) throws IOException {
 
         String contentType = request.getContentType();
