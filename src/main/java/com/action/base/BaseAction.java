6be914b09cc2283875c12c01e330411d0efc3503
wangyong <yundan_wy@126.com>
2017-09-22 23:46:23 +0800

    完善代码

--- src/main/java/com/action/base/BaseAction.java
+++ src/main/java/com/action/base/BaseAction.java
@@ -8,18 +8,23 @@ import java.io.IOException;
 public class BaseAction extends ActionSupport {
 
     protected String parseReqJsonData(HttpServletRequest request) throws IOException {
-        int contentLength = request.getContentLength();
-        if (contentLength < 0) {
-            return null;
-        }
-        byte buffer[] = new byte[contentLength];
-        for (int i = 0; i < contentLength; ) {
-            int len = request.getInputStream().read(buffer, i, contentLength - i);
-            if (len == -1) {
-                break;
+
+        String contentType = request.getContentType();
+        if (contentType.equals("application/json")) {
+            int contentLength = request.getContentLength();
+            if (contentLength < 0) {
+                return null;
             }
-            i += len;
+            byte buffer[] = new byte[contentLength];
+            for (int i = 0; i < contentLength; ) {
+                int len = request.getInputStream().read(buffer, i, contentLength - i);
+                if (len == -1) {
+                    break;
+                }
+                i += len;
+            }
+            return new String(buffer, "utf-8");
         }
-        return new String(buffer, "utf-8");
+        return null;
     }
 }
